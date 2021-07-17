994a42653e17ca09b8819ddd12b63920cacd29d8
GP <gregpetropoulos@yahoo.com>
2021-07-17 10:08:30 -0400

    Set up route for user git hub profile to call repos of a user with github api. I had to use PAT from github an axios for the call back in profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -257,8 +257,8 @@ router.put(
 // @access Private
 
 router.delete('/education/:edu_id', auth, async (req, res) => {
-    try {
-        const foundProfile = await Profile.findOne({ user: req.user.id });
+  try {
+    const foundProfile = await Profile.findOne({ user: req.user.id });
     foundProfile.education = foundProfile.education.filter(
       (edu) => edu._id.toString() !== req.params.edu_id
     );
@@ -271,4 +271,26 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   }
 });
 
+// @route   GET api/profile/github/:username
+// @desc    Gets users repos from github
+// @access Public
+
+router.get('/github/:username', async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      'user-agent': 'node.js',
+      Authorization: `token ${config.get('githubToken')}`,
+    };
+
+    const gitHubResponse = await axios.get(uri, { headers });
+
+    return res.json(gitHubResponse.data);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(404).json({ msg: 'No Github profile found' });
+  }
+});
 module.exports = router;
