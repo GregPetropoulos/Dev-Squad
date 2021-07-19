b45844e00d4cc879a9f92f16c2adffc8a1e46aac
GP <gregpetropoulos@yahoo.com>
2021-07-19 08:15:05 -0400

    Working on the reducers in index.js, store.js, and wrapped the Provider around the app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,30 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-
 import './App.css';
 
-const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
+// Redux need a provider to combine redux into react
+import { Provider } from 'react-redux';
 
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/register' component={ Register } />
-          <Route exact path='/login' component={ Login } />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+// Redux
+import store from './store';
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
