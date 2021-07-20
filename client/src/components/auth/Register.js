67e0778d3b65650e910f60e9c2abc184f3f7562a
GP <gregpetropoulos@yahoo.com>
2021-07-20 05:54:11 -0400

    Working through the Alert component  and action call with the redux tool to show the password warning on sign up page.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,13 +1,15 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom'
-
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
 
 
 // Commented out axios for the inline sign up request, will use redux instead
 // import axios from 'axios';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -23,7 +25,8 @@ const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Password do not match');
+      setAlert('Passwords do not match', 'danger')
+      // console.log('Password do not match');
     } else {
 console.log('SUCCESS', formData)
 
@@ -112,5 +115,7 @@ console.log('SUCCESS', formData)
     </Fragment>
   );
 };
-
-export default Register;
+Register.propTypes = { 
+  setAlert: PropTypes.func.isRequired,
+}
+export default connect(null, { setAlert })(Register);
