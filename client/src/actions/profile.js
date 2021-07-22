8c6b58acafaa022bb56393b496faea5a91e3292f
GP <gregpetropoulos@yahoo.com>
2021-07-22 12:18:53 -0400

    I created a  Profile reducer, actions. The  profile action  will make request when logged in and it will put all profile data into initialState array.

---
+++ client/src/actions/profile.js
@@ -0,0 +1,21 @@
+import api from '../utils/api';
+import { setAlert } from './alert';
+
+import { GET_PROFILE, PROFILE_ERROR } from './types';
+
+// Get current users profile
+export const getCurrentProfile = () => async (dispatch) => {
+  try {
+    const res = await api.get('api//profile/me');
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
---
+++ client/src/reducers/profile.js
@@ -0,0 +1,33 @@
+// profile will make request when logged in and it will put all profile data into initialState array
+
+import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {},
+};
+
+function profileReducer(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false,
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
+
+export default profileReducer;
