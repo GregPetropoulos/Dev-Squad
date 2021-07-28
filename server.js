3cb758bc3860e09e041a6f8ab2d3f19b6b847fdc
GP <gregpetropoulos@yahoo.com>
2021-07-27 21:31:11 -0400

    Prepared server and package.json for deployment.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express= require('express');
+const path = require ('path');
 
 // Database setup
 const connectDB =require ('./config/db');
@@ -12,14 +13,22 @@ connectDB();
 // Middleware
 app.use(express.json());
 
-// Test API
-app.get('/', (req, res) => res.send('API Running'));
 
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    // Set the static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res)=> {
+        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
+    })
+}
 
 
 const PORT = process.env.PORT || 5000;
